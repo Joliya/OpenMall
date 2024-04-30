440b5469ced4a8000a9adc6d3ff60ebc41abe3ad
zhangjinpeng <zhangjinpeng@huixuanjiasu.com>
2024-04-30 17:01:30 +0800

    增加sku 的增删改查

--- routers/sku/view.go
+++ routers/sku/view.go
@@ -51,3 +51,27 @@ func CreateSkuView(c *gin.Context) {
 	// 调用 sku_service.CreateSku
 	// 返回结果
 }
+
+func UpdateSkuView(c *gin.Context) {
+	// 将 c 中的数据解析为 SkuDto
+	sku := SkuDto{}
+	err := c.BindJSON(&sku)
+	if err != nil {
+		http_tools.JsonHttpError(c, "参数错误", nil)
+	}
+	// 调用 sku_service.UpdateSku
+	// 返回结果
+}
+
+func DeleteSkuView(c *gin.Context) {
+	skuId, exist := c.GetQuery("skuId")
+	if !exist {
+		http_tools.JsonHttpError(c, "商品不存在", nil)
+	}
+	_, err := strconv.Atoi(skuId)
+	if string_util.IsNotNil(err) {
+		http_tools.JsonHttpError(c, "商品不存在", nil)
+	}
+	// 调用 sku_service.DeleteSku
+	// 返回结果
+}
